1b8fd7167de90d01c11ec7d457c4fa6174880b41
KostyaShey <sheynkler87@gmail.com>
2023-01-13 21:41:36 +0100

    first experiments with svelte

--- legail/src/routes/+page.svelte
+++ legail/src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    import Catimage from "./Catimage.svelte";
+    let dimentions = {width:'500', height:'300'}
+</script>
+
+
+<h1>Welcome to Penis</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class='inputs'>
+    <input type="number" id='width' name='width' bind:value="{dimentions.width}">
+    <input type="number" id='width' name='width' bind:value="{dimentions.height}">
+</div>
+<div>
+    <Catimage width={dimentions.width} height={dimentions.height}/>
+</div>
+
+<style>
+    .inputs {
+  padding-bottom: 1rem;
+}
+</style>
---
+++ legail/src/routes/Catimage.svelte
@@ -0,0 +1,13 @@
+<script>
+	
+    export let width;
+    export let height;
+    $: src = 'https://placekitten.com/';
+    const alt = "Picture of a cat";
+
+</script>
+
+<img src='{src}{width}/{height}' {alt}>
+
+<p>width = {width}, height = {height}</p>
+<p>width type = {(typeof width)}, height = {height}</p>
