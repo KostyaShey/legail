8f8105488da5c936ec486f4484fabbfecf3c4fe6
KostyaShey <sheynkler87@gmail.com>
2023-02-02 11:34:29 +0100

    changed chat behaviour in logged in area

--- legail/src/routes/stores/chatSession.js
+++ legail/src/routes/stores/chatSession.js
@@ -2,21 +2,7 @@ import { readable, writable } from "svelte/store";
 
 
 export let chatMessages = writable([
-    {
-        id: "asjnfdoijnasoidfj",
-        userMessage: true,
-        text: "lorem ipsum dolores"
-    },
-    {
-        id: "asjnidfj",
-        userMessage: false,
-        text: "lorem ipsum dolores"
-    },
-    {
-        id: "asjn askjdhihasghid idfj",
-        userMessage: false,
-        text: "lorem ipsum kajsndfojnsdijkn dolores"
-    }
+
 ])
 
 export const chatInfo = readable(
